23f8998636ec42f05f153809c6310bda900477c2
Lavoisier Cornerstone <vois.cornerstone@gmail.com>
2013-11-14 09:47:59 -0500

    initial commit

---
+++ birthday.rb
@@ -0,0 +1,15 @@
+puts "When is your Birthday"
+birthday = gets.chomp
+
+if birthday == "November 14" || 11/14 || "today"
+	puts "Go shawty, it's ya birthday.... - 50 Cent"
+else
+	puts "And you know we don't really care its not your birthday!!!!! -50 Cent"
+end
+
+=begin
+when input is first condition, runs, but outpus the following error:
+warning: string literal in condition		
+When the wrong birthday is entered, it still gives the first condition
+=end
+
